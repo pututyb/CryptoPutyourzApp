b02aad249da05b7af40621e6b32860e0bc387f8e
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-09-16 19:24:40 +0700

    Initial Commit

---
+++ CryptoPutyourzApp/CryptoPutyourzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPutyourzAppApp.swift
+//  CryptoPutyourzApp
+//
+//  Created by Putut Yusri Bahtiar on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPutyourzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
